8a5b9953f07f0289905281f906cf1893bad1a97b
mathieuboudreau <emb6150@gmail.com>
2016-08-23 18:29:20 -0230

    Add empty data folder with required files to be included in future data folders

---
+++ data/study_info.m
@@ -0,0 +1,46 @@
+
+%% Study Info
+%
+
+
+%% Structural
+%
+
+structID= 75;
+
+%% Subject ID info
+%
+
+subjectID = 'alonso_eva_20131025_141318';
+
+%% B1
+%
+
+mb_gre_bsID = [11,13];
+mb_gre_bs_gaussID = [15,17];
+mb_clt_gre_bs_cr_fermiID = [19,21];
+mb_clt_gre_bs_crushID = [23,25];
+%clt_bsID = [9,10,11,12];
+
+
+epseg_daID=[76,77];
+bic_tseID = 3;
+clt_tseID =  [4,6];
+clt_afiID = [8,9];
+
+
+%% T1
+%
+
+gre_vfaID = [34,35];
+clt_vfaID = [30,31];
+clt_vfa_spoilID = [32,33];
+clt_tse_irID = [36,38,40,42];
+ep_segID = [46,47];
+
+
+%% B0
+%
+
+mtspgr_b0ID = 49;
+gre_b0ID = 51;
