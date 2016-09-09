72f0414fa3a50d89e1283f67d4758a9ff7af1239
mathieuboudreau <emb6150@gmail.com>
2016-09-09 14:12:01 -0230

    Integrate dirs2cells into imageB1T1cat

--- src/data_analysis/imageB1T1cat.m
+++ src/data_analysis/imageB1T1cat.m
@@ -1,34 +1,30 @@
-function [] = imageB1T1cat()
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [] = imageB1T1cat(dataDir)
+%imageB1T1cat ImageShow concatenated B1 & T1 maps
+%
+% --args--
+% dataDir: String forentire path to directory containing the folders of
+%          each subjects data.
+%          Example usage: dataDir = [pwd '/data'];
+
 %%
 %
-%% Final Image
-%
 
-
-subjectID{1}='arseneault_ryan_20131015_140558';
-subjectID{2}='caissie_jessica_20131015_152703';
-subjectID{3}='mazerolle_erin_20131016_152802';
-subjectID{4}='stikov_nikola_20131017_113942';
-subjectID{5}='collette_marc_20131024_093735';
-subjectID{6}='alonso_eva_20131025_141318';
-
+subjectID = dirs2cells(dataDir);
 
 for counterSubject = 1:length(subjectID)
-    [t1_hdr,t1{1}] = niak_read_minc([subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_tse.mnc']);
-    [~,t1{2}] = niak_read_minc([subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_bs.mnc']);
-    [~,t1{3}] = niak_read_minc([subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_afi.mnc']);
-    [~,t1{4}] = niak_read_minc([subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_epseg_da.mnc']);
+    [t1_hdr,t1{1}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_tse.mnc']);
+    [~,t1{2}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_bs.mnc']);
+    [~,t1{3}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_clt_afi.mnc']);
+    [~,t1{4}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/t1/t1_clt_vfa_spoil_b1_epseg_da.mnc']);
 
 
 
-    [~,b1{1}] = niak_read_minc([subjectID{counterSubject} '/b1_whole_brain/b1_clt_tse.mnc']);
-    [~,b1{2}] = niak_read_minc([subjectID{counterSubject} '/b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc']);
-    [~,b1{3}] = niak_read_minc([subjectID{counterSubject} '/b1_whole_brain/b1_clt_afi.mnc']);
-    [~,b1{4}] = niak_read_minc([subjectID{counterSubject} '/b1_whole_brain/b1_epseg_da.mnc']);
+    [~,b1{1}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/b1_whole_brain/b1_clt_tse.mnc']);
+    [~,b1{2}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc']);
+    [~,b1{3}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/b1_whole_brain/b1_clt_afi.mnc']);
+    [~,b1{4}] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/b1_whole_brain/b1_epseg_da.mnc']);
 
-    [~,mask] = niak_read_minc([subjectID{counterSubject} '/mask/mask.mnc']);
+    [~,mask] = niak_read_minc([dataDir '/' subjectID{counterSubject} '/mask/mask.mnc']);
 
     %%
     for ii=1:length(t1)
