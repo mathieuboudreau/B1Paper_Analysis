34a96082c17ee8ef84e0a0d224614ce028962744
mathieuboudreau <emb6150@gmail.com>
2016-09-10 18:18:32 -0230

    Move subfunction to it's own file

--- src/data_analysis/histT1B1.m
+++ src/data_analysis/histT1B1.m
@@ -131,31 +131,3 @@ function [] = histT1B1(dataDir, b1t1FileOptions)
     plotHistogram(xB1, yFreqB1, 'B_1 (n.u.)', 'a.u.', b1Keys, colours);
 
 end
-
-function plotHistogram(xData, yData, xLabel, yLabel, legendCells, lineColours)
-%PLOTHISTOGRAM Plot histogram data and format figure.
-%
-%   --args--
-%   xData: cells of the frequencies' xaxis values for each line.
-%   yData: cells of the histogram frequencies to be plot for each line.
-%   xLabel: string to be set as xlabel.
-%   yLabel: string to be set as ylabel.
-%   legendCells: cells of strings for the legend of the plot.
-%   lineColours: M-by-3 matrix containing a "ColorOrder" colormap.
-%
-    h.figure = figure();
-    h.axes   = gca;
-    for ii=1:length(yData)
-        plot(xData{ii},yData{ii}./sum(yData{ii}), '-', 'Color', lineColours(ii,:), 'LineWidth',4)
-        hold on
-    end
-
-    h.xlabel=xlabel(xLabel);
-    h.ylabel=ylabel(yLabel);
-
-    % Remove underscores from legends, and make the cell array the legend
-    legendCells = escapeUnderscores(legendCells);
-    h.legend=legend(legendCells);
-
-    plotFigureProperties(h);
-end
