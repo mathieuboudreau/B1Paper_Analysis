67ef765d50c6255ec353d3c3190e7bcfbbd40a12
mathieuboudreau <emb6150@gmail.com>
2016-11-03 17:17:07 -0230

    Comment out legacy script, will add one section at a time properly

--- src/scripts/blurAllSubjects.m
+++ src/scripts/blurAllSubjects.m
@@ -8,104 +8,107 @@ clear all
 clc
 close all
 
-%% Make directories
-%
-
-olddir = cd;
-
-%cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/alonso_eva_20131025_141318')
-%cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/arseneault_ryan_20131015_140558')
-%cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/caissie_jessica_20131015_152703')
-%cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/collette_marc_20131024_093735')
-%cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/mazerolle_erin_20131016_152802')
-cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/stikov_nikola_20131017_113942')
-
-
-%% Load study info
-%
-
-% **Needs to be manually modified for each scans/protocol**
-study_info
-
-%% Calculate B1 maps
-%
-
-% Blur
-mkdir b1_blur
-mkdir b1_spline
-
-system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_tse.mnc b1_blur/b1_clt_tse' ]);
-system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_afi.mnc b1_blur/b1_clt_afi' ]);
-system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc b1_blur/b1_clt_gre_bs_cr_fermi' ]);
-system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_epseg_da.mnc b1_blur/b1_epseg_da' ]);
-
-
-% Spline
-
-system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_tse.mnc b1_spline/b1_clt_tse_spline.mnc'])
-system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_afi.mnc b1_spline/b1_clt_afi_spline.mnc'])
-system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc'])
-system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_epseg_da.mnc b1_spline/b1_epseg_da_spline.mnc'])
-
-%%
-%
-
-[~, blur{1}]=niak_read_minc('b1_blur/b1_clt_tse_blur.mnc');
-[~, blur{2}]=niak_read_minc('b1_blur/b1_clt_afi_blur.mnc');
-[~, blur{3}]=niak_read_minc('b1_blur/b1_clt_gre_bs_cr_fermi_blur.mnc');
-[~, blur{4}]=niak_read_minc('b1_blur/b1_epseg_da_blur.mnc');
-
-[~, spline{1}]=niak_read_minc('b1_spline/b1_clt_tse_spline.mnc');
-[~, spline{2}]=niak_read_minc('b1_spline/b1_clt_afi_spline.mnc');
-[~, spline{3}]=niak_read_minc('b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc');
-[~, spline{4}]=niak_read_minc('b1_spline/b1_epseg_da_spline.mnc');
-
-[~,mask]=niak_read_minc('t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc');
-
-
-for ii=1:4
-    figure(),imagesc(blur{ii}.*mask),caxis([0.7 1.2])
-    figure(),imagesc(spline{ii}.*mask),caxis([0.7 1.2])
-end
-
-%% Fit T1 maps
-%
-
-mkdir t1_blur
-mkdir t1_spline
-
-% VFA Optimum Spoil Blur
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_afi_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_afi_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_tse_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_tse_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_epseg_da_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_epseg_da_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_gre_bs_cr_fermi_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_bs_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-% VFA Optimum Spoil Spline
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_afi_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_afi_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_tse_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_tse_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_epseg_da_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_epseg_da_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
-!rm VFA_3.mnc
-!rm VFA_20.mnc
-fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_bs_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
-
+% 
+% 
+% 
+% %% Make directories
+% %
+% 
+% olddir = cd;
+% 
+% %cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/alonso_eva_20131025_141318')
+% %cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/arseneault_ryan_20131015_140558')
+% %cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/caissie_jessica_20131015_152703')
+% %cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/collette_marc_20131024_093735')
+% %cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/mazerolle_erin_20131016_152802')
+% cd('/Users/mathieuboudreau/Work/Projects/B1smoothing/BlurHistComp/ISMRM/stikov_nikola_20131017_113942')
+% 
+% 
+% %% Load study info
+% %
+% 
+% % **Needs to be manually modified for each scans/protocol**
+% study_info
+% 
+% %% Calculate B1 maps
+% %
+% 
+% % Blur
+% mkdir b1_blur
+% mkdir b1_spline
+% 
+% system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_tse.mnc b1_blur/b1_clt_tse' ]);
+% system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_afi.mnc b1_blur/b1_clt_afi' ]);
+% system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc b1_blur/b1_clt_gre_bs_cr_fermi' ]);
+% system(['mincblur -no_apodize -dim 2 -fwhm 10 b1_whole_brain/b1_epseg_da.mnc b1_blur/b1_epseg_da' ]);
+% 
+% 
+% % Spline
+% 
+% system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_tse.mnc b1_spline/b1_clt_tse_spline.mnc'])
+% system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_afi.mnc b1_spline/b1_clt_afi_spline.mnc'])
+% system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc'])
+% system(['spline_smooth -verbose -distance 60 -lambda 2.5119e-06 -mask t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc b1_whole_brain/b1_epseg_da.mnc b1_spline/b1_epseg_da_spline.mnc'])
+% 
+% %%
+% %
+% 
+% [~, blur{1}]=niak_read_minc('b1_blur/b1_clt_tse_blur.mnc');
+% [~, blur{2}]=niak_read_minc('b1_blur/b1_clt_afi_blur.mnc');
+% [~, blur{3}]=niak_read_minc('b1_blur/b1_clt_gre_bs_cr_fermi_blur.mnc');
+% [~, blur{4}]=niak_read_minc('b1_blur/b1_epseg_da_blur.mnc');
+% 
+% [~, spline{1}]=niak_read_minc('b1_spline/b1_clt_tse_spline.mnc');
+% [~, spline{2}]=niak_read_minc('b1_spline/b1_clt_afi_spline.mnc');
+% [~, spline{3}]=niak_read_minc('b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc');
+% [~, spline{4}]=niak_read_minc('b1_spline/b1_epseg_da_spline.mnc');
+% 
+% [~,mask]=niak_read_minc('t1_whole_brain/t1_clt_vfa_spoil_b1_clt_tse_mask.mnc');
+% 
+% 
+% for ii=1:4
+%     figure(),imagesc(blur{ii}.*mask),caxis([0.7 1.2])
+%     figure(),imagesc(spline{ii}.*mask),caxis([0.7 1.2])
+% end
+% 
+% %% Fit T1 maps
+% %
+% 
+% mkdir t1_blur
+% mkdir t1_spline
+% 
+% % VFA Optimum Spoil Blur
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_afi_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_afi_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_tse_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_tse_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_epseg_da_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_epseg_da_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_blur/b1_clt_gre_bs_cr_fermi_blur.mnc', 't1_blur/t1_clt_vfa_spoil_b1_clt_bs_blur', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% % VFA Optimum Spoil Spline
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_afi_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_afi_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_tse_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_tse_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_epseg_da_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_epseg_da_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
+% !rm VFA_3.mnc
+% !rm VFA_20.mnc
+% fitDataVFA_es (subjectID, clt_vfa_spoilID, 'b1_spline/b1_clt_gre_bs_cr_fermi_spline.mnc', 't1_spline/t1_clt_vfa_spoil_b1_clt_bs_spline', [subjectID, '_', num2str(structID), '_mri_reg_resamp_es.mnc']) 
+% 
