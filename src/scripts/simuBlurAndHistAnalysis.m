8d78446e6203fad5cf42fa1c720d7fa2626937cc
mathieuboudreau <emb6150@gmail.com>
2016-11-04 11:49:47 -0230

    Create new b1 map with a sumperimposed sinusoidal artefact

--- src/scripts/simuBlurAndHistAnalysis.m
+++ src/scripts/simuBlurAndHistAnalysis.m
@@ -18,6 +18,8 @@ imDims = [100, 100];
 diagionalRotationAngle = 90;
 b1MapRescaleFactor = 0.5;
 
+sinB1MapAmpl = 0.2;
+sinB1MapFreq = 1;
 %% Generate ideal "Ref" B1 map with diagonal gradient
 %
 
@@ -33,3 +35,10 @@ figIndex = figIndex+1; figure(figIndex), imagesc(b1Map_ref), axis image, set(gca
 
 % Cleanup variables that are of no use anymore
 clear b1map_tmp diagionalGrad
+
+%% Create a b1map with sinusoid superimpose to simulate a regular imprecision or artefact
+%
+
+sinB1Map = b1Map_ref + sinB1MapAmpl * sin(sinB1MapFreq * Xgrad);
+
+figIndex = figIndex+1; figure(figIndex), imagesc(sinB1Map), axis image, set(gca, 'XTick', [], 'YTick', []), colormap(jet), title('Sin B_1 Map', 'FontSize', 24, 'FontName', 'TimesNewRoman', 'FontWeight', 'normal')
