734574779451abb7397939332b74aa50b7253a40
mathieuboudreau <emb6150@gmail.com>
2016-11-04 11:14:53 -0230

    Initial commit of new script

---
+++ src/scripts/simuBlurAndHistAnalysis.m
@@ -0,0 +1,14 @@
+%% simuBlurAndHistAnalysis
+% Exploratory analysis of to explore why histograms and correlations were 
+% used for the paper analysis
+%
+
+%% Initial setup
+%
+
+clear all
+close all
+clc
+
+%%
+%
