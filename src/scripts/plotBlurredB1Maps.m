e077697cad0a1779e56d610f140976adaa0b8c27
mathieuboudreau <emb6150@gmail.com>
2016-11-11 18:40:23 -0330

    Update script for 95th percentile analysis of WM masked blurred B1 maps

--- src/scripts/plotBlurredB1Maps.m
+++ src/scripts/plotBlurredB1Maps.m
@@ -15,13 +15,13 @@ DEBUG=0;
 %
 
 dataDir = [pwd '/data'];
-
 maskFile = 'brain_mask_es_2x2x5.mnc';
 
 subjectIDs = dirs2cells(dataDir);
 numSubjects = size(subjectIDs,1);
 
 customBWRmap = createBWRcolormap();
+WMFlag = 1;
 
 %%
 %
@@ -31,17 +31,28 @@ oldDir = cd;
 for counterSubject = 1:numSubjects
     cd([dataDir '/' subjectIDs{counterSubject}])
 
-    [~, b1{1}]=niak_read_minc('b1_whole_brain/b1_clt_tse.mnc');
-    [~, b1{2}]=niak_read_minc('b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc');
-    [~, b1{3}]=niak_read_minc('b1_whole_brain/b1_clt_afi.mnc');
-    [~, b1{4}]=niak_read_minc('b1_whole_brain/b1_epseg_da.mnc');
+    if(WMFlag == 1)
+        [~, b1{1}]=niak_read_minc('b1/b1_clt_tse.mnc');
+        [~, b1{2}]=niak_read_minc('b1/b1_clt_gre_bs_cr_fermi.mnc');
+        [~, b1{3}]=niak_read_minc('b1/b1_clt_afi.mnc');
+        [~, b1{4}]=niak_read_minc('b1/b1_epseg_da.mnc');
 
+        [~, blur{1}]=niak_read_minc('b1_gauss_wm/b1_clt_tse.mnc');
+        [~, blur{2}]=niak_read_minc('b1_gauss_wm/b1_clt_gre_bs_cr_fermi.mnc');
+        [~, blur{3}]=niak_read_minc('b1_gauss_wm/b1_clt_afi.mnc');
+        [~, blur{4}]=niak_read_minc('b1_gauss_wm/b1_epseg_da.mnc');
+    else
+        [~, b1{1}]=niak_read_minc('b1_whole_brain/b1_clt_tse.mnc');
+        [~, b1{2}]=niak_read_minc('b1_whole_brain/b1_clt_gre_bs_cr_fermi.mnc');
+        [~, b1{3}]=niak_read_minc('b1_whole_brain/b1_clt_afi.mnc');
+        [~, b1{4}]=niak_read_minc('b1_whole_brain/b1_epseg_da.mnc');
 
-    [~, blur{1}]=niak_read_minc('b1_gauss/b1_clt_tse.mnc');
-    [~, blur{2}]=niak_read_minc('b1_gauss/b1_clt_gre_bs_cr_fermi.mnc');
-    [~, blur{3}]=niak_read_minc('b1_gauss/b1_clt_afi.mnc');
-    [~, blur{4}]=niak_read_minc('b1_gauss/b1_epseg_da.mnc');
-
+        [~, blur{1}]=niak_read_minc('b1_gauss/b1_clt_tse.mnc');
+        [~, blur{2}]=niak_read_minc('b1_gauss/b1_clt_gre_bs_cr_fermi.mnc');
+        [~, blur{3}]=niak_read_minc('b1_gauss/b1_clt_afi.mnc');
+        [~, blur{4}]=niak_read_minc('b1_gauss/b1_epseg_da.mnc');
+    end
+    
     [~,mask]=niak_read_minc(maskFile);
 
     mask = logical(mask);
@@ -82,7 +93,37 @@ for counterSubject = 1:numSubjects
     end
 
     figure(counterSubject*100 + 4), imagesc([pDiffRelRef{1:4}]), caxis([-10 10]), colormap(customBWRmap),  axis image
-
+    
+    %% For 95th percentiles
+    subjPDiffRelRef{counterSubject} = pDiffRelRef;
+    refBllurredB1{counterSubject} = rawB1{1};
 end
+
+%% 95th percentiles
+%
+nominal = ones(size(blurB1{1}));
+pooledBlurredB1=cell(1,4);
+pDiffNominalRelRef=[];
+for counterSubject = 1:6
+    for ii = 1:4
+       pooledBlurredB1{ii} = [pooledBlurredB1{ii}; subjPDiffRelRef{counterSubject}{ii}(:)]; 
+    end
+    
+    pDiffNominalRelRef = [pDiffNominalRelRef; (refBllurredB1{counterSubject}-nominal)./refBllurredB1{counterSubject}.*100];
+end
+
+for ii = 1:4
+    pooledBlurredB1{ii}(isnan(pooledBlurredB1{ii}))=[];
+    pooledBlurredB1{ii}(isinf(pooledBlurredB1{ii}))=[];
+    pooledBlurredB1{ii}(abs(pooledBlurredB1{ii}-100)<0.01)=[];
+
+    prctile(abs(pooledBlurredB1{ii}),95)
+end
+
+pDiffNominalRelRef(isnan(pDiffNominalRelRef))=[];
+pDiffNominalRelRef(isinf(pDiffNominalRelRef))=[];
+pDiffNominalRelRef(abs(pDiffNominalRelRef-100)<0.01)=[];
+
+prctile(abs(pDiffNominalRelRef),95)
 
 cd(oldDir)
